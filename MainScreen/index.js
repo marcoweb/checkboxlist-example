ac2031f25ba9752725dad30261556d437b67dfda
Marco Carvalho <marcocarvalho.web@gmail.com>
2022-07-21 12:04:42 -0300

    Lista com Firebase

--- MainScreen/index.js
+++ MainScreen/index.js
@@ -1,32 +1,49 @@
 import { View, Text, FlatList, TextInput, Button, TouchableHighlight, Alert } from "react-native";
 import { useEffect, useState } from "react";
+import { query, collection, getDocs, addDoc, doc, setDoc, deleteDoc } from "firebase/firestore"
+import { firestore } from '../firebase'
 import Checkbox from 'expo-checkbox'
 
 import { styles } from './styles'
 
 const MainScreen = () => {
-    const [ tarefas, setTarefas ] = useState([
-        { id: 1, rotulo: 'Tarefa 1', checked: false },
-        { id: 2, rotulo: 'Tarefa 2', checked: false },
-        { id: 3, rotulo: 'Tarefa 3', checked: false }
-    ])
+    const [ tarefas, setTarefas ] = useState([])
     const [ tarefaEmEdicao, setTarefaEmEdicao ] = useState({ id: 0, rotulo: '' })
+
+    async function loadTarefas() {
+        try {
+            const q = query(collection(firestore, "tarefas"))
+            const queryResult = await getDocs(q)
+            let items = []
+            queryResult.forEach((doc) => {
+                items.push({
+                    id: doc.id,
+                    rotulo: doc.data().rotulo,
+                    status: doc.data().status
+                })
+            })
+            setTarefas(items)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function setTarefaCheck(tarefa) {
+        try {
+            await setDoc(doc(firestore, "tarefas", tarefa.id), {status: !tarefa.status}, {merge: true})
+            loadTarefas()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     function limpaTarefaEmEdicao() {
         setTarefaEmEdicao({ id: 0, rotulo: '' })
     }
 
-    function setCheckbox(id) {
-        let list = []
-        tarefas.forEach((item) => {
-          if(item.id === id) {
-            list.push({id: item.id, rotulo: item.rotulo, checked: !item.checked})
-          } else {
-            list.push(item)
-          }
-        })
-        setTarefas(list)
-      }
+    useEffect(() => {
+        loadTarefas()
+    }, [])
 
     return (
         <View style={styles.container}>
@@ -45,7 +62,7 @@ const MainScreen = () => {
             </View>
             <FlatList data={tarefas} renderItem={({ item }) =>
                 <View style={styles.taskContainer}>
-                    <Checkbox value={item.checked} onValueChange={() => setCheckbox(item.id)} />
+                    <Checkbox style={{height: 30, width: 30}} value={item.status} onValueChange={() => setTarefaCheck(item)} />
                     <TouchableHighlight onPress={() => setTarefaEmEdicao(item)} onLongPress={() => deleteTarefa(item)}>
                         <Text style={styles.task} >{item.rotulo}</Text>
                     </TouchableHighlight>
